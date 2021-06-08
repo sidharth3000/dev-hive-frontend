aef4111dab508a457402416de23de495576cd2c0
sidharth <201952234@iiitvadodara.ac.in>
2021-06-09 03:31:39 +0530

    comments added

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -6,6 +6,48 @@ import classes from './Post.module.css';
 
 class Post extends Component { 
 
+    state = {
+        comment: ""
+    }
+
+    onCommenrchangeHandler = (event) => {
+        this.setState({comment: event.target.value})
+    }
+
+    onCommnetHandleer = (ev) => {
+
+        console.log(ev.target.id)
+
+        let config = {
+            headers: {
+                token: localStorage.getItem('token')
+            }
+        }
+
+        var d = new Date(),
+        minutes = d.getMinutes().toString().length === 1 ? '0'+d.getMinutes() : d.getMinutes(),
+        hours = d.getHours().toString().length === 1 ? '0'+d.getHours() : d.getHours(),
+        ampm = d.getHours() >= 12 ? 'pm' : 'am',
+        months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+        days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+        const time =  days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+
+        const data = {
+            body: this.state.comment,
+            time: d,
+            post: ev.target.id,
+            name: localStorage.getItem('name')
+        }
+
+        axios.post('http://localhost:9000/comment', data, config )
+        .then((res) => {
+            console.log(res.data)
+        })
+        .catch((event) => {
+            console.log(event)
+        })
+    }
+
     onPostDeletehandler = (e) => {
 
         let config = {
@@ -39,7 +81,7 @@ class Post extends Component {
                     <img src={`data:image/jpg;base64,${this.props.profile}`} className={styles.user}></img>
 
                     <div className={styles.info}>
-                        <div className={styles.name}>{localStorage.getItem('name')} <span style={{"color": "rgb(141, 141, 141)", "fontSize": "15px"}}>posted an update</span></div>
+                        <div className={styles.name}>{this.props.name} <span style={{"color": "rgb(141, 141, 141)", "fontSize": "15px"}}>posted an update</span></div>
                         <div className={styles.time}>{this.props.time}</div>
                     </div>
 
@@ -56,9 +98,19 @@ class Post extends Component {
 
                 <div>
                     <div className={styles.number}>{this.props.likes}</div>
-                    <button className={styles.like}><i className="fa fa-thumbs-up"></i></button>
+                    <button className={styles.like}><i className="fa fa-heart"></i></button>
                     <button className={styles.full}>View Full Post</button>
-                    <div className={styles.delete} key={this.props.id} id={this.props.id} onClick={this.onPostDeletehandler}>Delete</div>
+<br/>
+                    <div className={styles.cmmnt_cont}>
+                        <input type="text" className={styles.cmmnt} placeholder="comment" onChange={this.onCommenrchangeHandler}></input>
+                        <div className={styles.send}  onClick={this.onCommnetHandleer}><i class="fa fa-arrow-circle-right" id={this.props.id}></i></div>
+                    </div>
+
+                    {this.props.delete ?
+                    <div className={styles.delete} id={this.props.id} onClick={this.onPostDeletehandler}>Delete Post</div>
+                    :
+                    null
+                    }
 
                 </div>
             </div>
--- src/container/Feed/Feed.js
+++ src/container/Feed/Feed.js
@@ -3,17 +3,63 @@ import React, {Component} from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import Head from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Post from './../../components/Post/Post'
+import axios from 'axios';
+import styles from './Feed.module.css'
 
 class Feed extends Component {
 
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+
+        let config = {
+            headers: {
+              token: localStorage.getItem("token")
+            }
+        }
+
+        axios.get('http://localhost:9000/posts', config)
+        .then((res) => {
+            this.setState({posts: res.data})
+            console.log(res.data)
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+    }
+
     render(){
+
+        let posts =  <div >
+                        {this.state.posts.map(post =>(
+                            <Post 
+                            key={post._id}
+                            id={post._id}
+                            title={post.title}
+                            body={post.body}
+                            time={post.time}
+                            likes={post.likes}
+                            photo = {post.photo}
+                            profile = {post.owner.avatar}
+                            name = {post.owner.name}
+                            
+                            />
+                        ))}
+                    </div>
 
         return(
             <React.Fragment>
                 <Navbar/>
 
                 <Head>FEED</Head>
-                <div>Home</div>
+                <div className={styles.posts}>
+                    <div className={styles.my_posts}>Updates</div>
+                    {posts}
+                </div>
+               
 
                 <Footer/>
             </React.Fragment>
